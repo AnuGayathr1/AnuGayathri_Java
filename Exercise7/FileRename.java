a9e2985cc6c579f0117d8d652b0468440e59eca6
AnuGayathr1 <125632013+AnuGayathr1@users.noreply.github.com>
2023-03-10 13:25:54 +0530

    Update FileRename.java

--- Exercise7/FileRename.java
+++ Exercise7/FileRename.java
@@ -3,14 +3,14 @@ package Exercise7;
 import java.io.File;
 
 public class FileRename {
-public static void main(String[] args) {
-	File name=new File("/Users/anugayathri/eclipse-workspace/Exercise7/name");
-	File rename=new File("/Users/anugayathri/eclipse-workspace/Exercise7/rename");
-	if(name.renameTo(rename)) {
-		System.out.println("File renamed successfully...");
-	}else {
-		System.out.println("Failed to rename the file...");
+	public static void main(String[] args) {
+		File name = new File("/Users/anugayathri/eclipse-workspace/Exercise7/name");
+		File rename = new File("/Users/anugayathri/eclipse-workspace/Exercise7/rename");
+		if (name.renameTo(rename)) {
+			System.out.println("File renamed successfully...");
+		} else {
+			System.out.println("Failed to rename the file...");
+		}
 	}
-}
 }
 

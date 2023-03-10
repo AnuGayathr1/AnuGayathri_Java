329818073dfefb241511a7a92e99efdcbcb02881
AnuGayathr1 <125632013+AnuGayathr1@users.noreply.github.com>
2023-03-10 13:22:50 +0530

    Update CountOfWords.java

--- Exercise7/CountOfWords.java
+++ Exercise7/CountOfWords.java
@@ -2,16 +2,16 @@ package Exercise7;
 import java.util.*;
 public class CountOfWords {
 public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		String s=sc.nextLine();
+		Scanner scanner=new Scanner(System.in);
+		String string=scanner.nextLine();
 		int count=1;
-		for(int i=0;i<s.length()-1;i++) {
-			if(s.charAt(i)==' ') {
+		for(int i=0;i<string.length()-1;i++) {
+			if(string.charAt(i)==' ') {
 				count++;
 			}
 		}
 		System.out.println("Count of number of words: "+count);
 	}
-	
 }
+
 

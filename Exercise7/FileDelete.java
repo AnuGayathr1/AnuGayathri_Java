e91eb29871303d6996b398701112c970b7c70c5a
AnuGayathr1 <125632013+AnuGayathr1@users.noreply.github.com>
2023-03-10 13:25:03 +0530

    Update FileDelete.java

--- Exercise7/FileDelete.java
+++ Exercise7/FileDelete.java
@@ -3,12 +3,13 @@ package Exercise7;
 import java.io.File;
 
 public class FileDelete {
-public static void main(String[] args) {
-	File file=new File("/Users/anugayathri/eclipse-workspace/Exercise7/filetodelete");
-	if(file.delete()) {
-		System.out.println("File deleted successfully...");
-	}else {
-		System.out.println("Failed to delete the file...");
+	public static void main(String[] args) {
+		File file = new File("/Users/anugayathri/eclipse-workspace/Exercise7/filetodelete");
+		if (file.delete()) {
+			System.out.println("File deleted successfully...");
+		} else {
+			System.out.println("Failed to delete the file...");
+		}
 	}
 }
-}
+

4bfea5e1edf61aff8e84035190ce76918b237eaf
AnuGayathr1 <125632013+AnuGayathr1@users.noreply.github.com>
2023-03-10 13:23:22 +0530

    Update DateObject.java

--- Exercise7/DateObject.java
+++ Exercise7/DateObject.java
@@ -5,21 +5,21 @@ import java.util.Date;
 
 public class DateObject {
 	public static void main(String[] args) {
-		Date date=new Date();
-		String string=date.toString();
-		try(FileWriter filewriter=new FileWriter("date.txt");)
-		{
-			filewriter.write(date.toString());
+		Date date = new Date();
+		String string = date.toString();
+		try {
+			FileWriter fileWriter = new FileWriter("date.txt");
+			fileWriter.write(date.toString());
 			System.out.println("Written date to file successfully...");
-		}catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println("Failed to write date object to file...");
 		}
-		try(FileReader filereader=new FileReader("date.txt");
-				BufferedReader bufferedreader=new BufferedReader(filereader);){
-			System.out.println(bufferedreader.readLine());
-		}
-		catch(Exception e) {
+		try {
+			FileReader fileReader = new FileReader("date.txt");
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			System.out.println(bufferedReader.readLine());
+		} catch (Exception e) {
 			System.out.println("Failed to read date object from file...");
 		}
-}
+	}
 }
